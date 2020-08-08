1777c909f2e7e098f567db2f147bdd470cabf063
Dominik <kolasadom@gmail.com>
2020-08-08 14:37:00 +0200

    Add simple mail sending
    - sending mail with request data via REST API

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class SendMailView(APIView):
+    def post(self, request):
+        message = request.data.get('message')
+        send_mail(
+            'New email message title',
+            message,
+            from_email=settings.EMAIL_FROM,
+            recipient_list=[request.data.get('recipient')]
+        )
+        return Response('OK', status=status.HTTP_200_OK)
--- ring/urls.py
+++ ring/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from notifications.views import SendMailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mails/send', SendMailView.as_view())
 ]
