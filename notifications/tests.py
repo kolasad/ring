dd9ed5f4218f2f31deb2405b1fd50d0245281cb5
Dominik <kolasadom@gmail.com>
2020-08-09 09:42:41 +0200

    Finished notifications tests

--- notifications/tests.py
+++ notifications/tests.py
@@ -3,21 +3,24 @@ from django.test import TestCase, Client
 
 
 class SendMailTestCase(TestCase):
+    def setUpClasss(self):
+        self.client = Client()
+
     def test_send_mail(self):
         data = {'message': 'asd', 'recipient': 'test@test.com'}
 
-        client = Client()
-        response = client.post('/mails/send', data)
+        response = self.client.post('/mails/send', data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'New email message title')
 
+    def test_fail_invalid_data_send_mail(self):
+        data = {'message': 'Hello'}
 
-# def test_fail_invalid_data_send_mail():
-#     data = {'message': 'Hello'}
-#     # TODO wysylka maila
-#     # sprawdzamy czy dostalismy 400 i odpwiednia messsage w odpwiedzi
+        response = self.client.post('/mails/send', data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(len(mail.outbox), 0)
 
-
-# TODO ORM, ile jest kwiatow o takiej samej nazwie,
-# ile jest kwiatow o takiej samej nazwie i kolorze
+        response = self.client.post('/mails/send')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(len(mail.outbox), 0)
